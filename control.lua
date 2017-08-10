376d190c4927dce0330282812fb24c86dfb8f0ee
d3x0r <d3x0r@users.noreply.github.com>
2017-08-10 09:06:12 -0700

    Allow crafting ghosts and items on ground

--- control.lua
+++ control.lua
@@ -1,6 +1,8 @@
 
+local item_recipe = {};
 
 --[[
+
 local function _log_keys(prefix,object)
 	for _, __ in pairs(object) do
 		log( prefix.._.."="..tostring(__) );
@@ -24,6 +26,30 @@ local function log_keys(object)
 end
 ]]
 
+function init_item_recipe_map() 
+	--log( "Init item_recipe map" );
+	for rname,recipe in pairs( game.recipe_prototypes ) do
+		if( recipe.category == "crafting" or recipe.category=="electronics" ) then
+			--log( "Storing recipe:"..recipe.name .. " type:".. recipe.category);
+			--log_keys( recipe );
+			if not recipe.hidden then
+				for num,product in pairs(recipe.products) do
+					if item_recipe[product.name] then
+						--log( "Duplicate recipe for the item.".. product.name.. " to be "..recipe.name .. " was "..item_recipe[product.name] );
+						item_recipe[product.name][#item_recipe[product.name]+1] = recipe;
+					else
+						item_recipe[product.name] = { recipe };
+					end
+				end
+			else
+				--log( "Skipping recipe hidden:"..recipe.name);
+			end
+		else
+			--log( "Skipping recipe:"..recipe.name .. " type:".. recipe.category);
+		end
+	end
+end
+
 function count_item_type( allItems, itemName, at_item )
 	local count = 0;
 	-- count all in this level....
@@ -46,9 +72,17 @@ end
 
 function enough_items( items, itemName, n, player, allItems ) 
 	--log( "Check if enough items for a product of recipe:".. itemName );
-	for rname,recipe in pairs( game.recipe_prototypes ) do
-		if( recipe.category == "crafting" or recipe.category=="electronics" ) then
-			if not recipe.hidden then
+	local recipes = item_recipe[itemName];
+	local i;
+	local result;
+	if not recipes then
+		--log( "Failed to get recipes for item:"..itemName );
+		return {error="cannot-hand-craft", recipe=nil};
+	end
+	for i=1,#recipes do
+		local recipe = recipes[i]
+			--log( "recipe:".. recipe.name.. " cat:".. recipe.category.." enable:"..tostring(recipe.hidden))
+			if player.force.recipes[recipe.name].enabled then
 				for num,product in pairs(recipe.products) do
 					--log( "is "..product.name.."=="..itemName );
 					if( product.name == itemName ) then		
@@ -60,6 +94,8 @@ function enough_items( items, itemName, n, player, allItems )
 						for _,ingred in pairs( recipe.ingredients ) do	
 							--log( "   Ingredient:".. ingred.name );
 							local pcount = player.get_item_count( ingred.name );
+							--log( "is all items the same as items?".. tostring(allItems == items) );
+							if allItems == items then pamount = 1 end -- override pcount for root item
 							local needed = count_item_type( allItems, ingred.name, items );
 							--log( "needed to this point:".. needed .. " and ".. ingred.amount*n/pamount  );
 							--if  pcount < (needed + ingred.amount*n) then
@@ -76,24 +112,18 @@ function enough_items( items, itemName, n, player, allItems )
 						end
 					end
 				end
+				return { error=nil, recipe=recipe };
 			else
 				--log( "Skipping recipe:"..recipe.name );
+				return { error="recipe-locked" };
 			end
-		else
-			--for num,product in pairs(recipe.products) do
-			--	if( product.name == itemName ) then
-			--		log( "Skipped Otherwise good recipe!"..itemName .. " ".. recipe.category );
-			--	end
-			--end
-			
-			--log( "Skipped Recipe cat:"..recipe.category.. " "..recipe.name );
-		end
-	end	
+		--end
+	end
 end
 
 function resolve_needs( allItems ) 
 	local enough = true;
-	--log( "resolve all items in:".. tostring(allItems ) );
+	--log( "resolve all items in:".. tostring(allItems ).. "first:"..tostring(next(allItems)) );
 	for itemName, missing in pairs( allItems ) do
 		--log( "missing: "..itemName .. " #:"..missing.count);
 		if( missing.count > 0 )	 then
@@ -152,157 +182,130 @@ function formatMissing( missing, player, sub )
 	return lines;
 end
 
+function doWork( findname, n, player )
+	local missing_items={};
+	--log( "Find if I can make a "..findname );
+	local result = enough_items( missing_items, findname, n, player, missing_items );
+	if result.error then
+		--log( "enough Items return error:"..result.error );
+		--player.print( { "cannot-hand-craft" } );
+		return result;
+	end
+	if not resolve_needs( missing_items ) then
+		local items = formatMissing( missing_items, player, 0 );
+		if( items ) then
+			--player.print( { "missing-ingredient" } );
+			return { error = "missing-ingredient", detail=items, recipe=nil };
+		end
+	end
+	return result;
+end  
+
 function findRecipe( findname, n, player ) 
 	local missing_items={};
 	--log( "SEARCH for"  .. findname )
---[[
-	local recipe = game.recipe_prototypes[findname];
-	if recipe then
-		if recipe.hidden and not recipe.hidden then
-			log( "Skipping recipe:"..recipe.name );
-			log( "Hidden recipe" );
-			return nil;
-		end
-		for _,ingred in pairs( recipe.ingredients ) do
-			if( ingred.fluid ) then
-				player.print( { "cannot-craft-fluid" } );
-				return nil;
-			end
-			local pcount = player.get_item_count( ingred.name );	
-			--if pcount < (ingred.amount*n) then
-			missing_items[ingred.name] = { count= pcount-(ingred.amount*n), needed=(ingred.amount*n), subItems={} };
-		end
-
-
-
-		if #missing_items then
-			for name,missing in pairs( missing_items ) do
-				enough_items( missing.subItems, name, missing.count, player, missing_items );
-			end
-			player.print( { "missing-ingredient" } );
-			local items = formatMissing( missing_items, player );
-			log_keys( { "missing-item", {"item-name.".."rail"}, " x", 1,"\n" } );
-	                log_keys(items);
-			log( " got "..#items );
-			player.print( items );
-			return nil;
-		end
-		log( "return found name as original" );
-		return findname;
-	end
-]]
-	local finditem = nil;
-	
+	local result;
 	if game.entity_prototypes[findname].items_to_place_this then
+		--log( "More than one??" );
+		--for _,item in pairs( game.entity_prototypes[findname].items_to_place_this ) do
+		--	log( " item to place:".._.." it:"..item.name );
+		--end
 		for _,item in pairs( game.entity_prototypes[findname].items_to_place_this ) do
-				--log( "Item Tier:"..item.name.." T:"..tostring(item.tier) );
-				for rname,recipe in pairs( game.recipe_prototypes ) do
-					--log( "CHeck recipe:"..rname );
-					if not recipe.hidden and ( recipe.category=="crafting" or recipe.category=="advanced-crafting") then						
-						for num,product in pairs(recipe.products) do
-							if( product.name == item.name ) then
-								local pamount = product.amount;
-								if( not pamount ) then
-									pamount = product.max;
-								end
-								--log( "Found item : ".. item.name );
-								local failed = false;
-								for _,ingred in pairs( recipe.ingredients ) do	
-									if( ingred.fluid ) then
-										player.print( { "cannot-craft-fluid" } );
-										return nil;
-									end
-									--local pcount = player.get_item_count( ingred.name );
-									--if pcount < (ingred.amount*n) then
-									missing_items[ingred.name] = { count= (ingred.amount*n)-player.get_item_count( ingred.name ), need=(ingred.amount*n), subItems={} };									
-									--end
-								end
-								if next(missing_items) then
-									for name,missing in pairs( missing_items ) do
-										--log( "See if we have enough items for:".. name.. " ".. missing.count );
-										enough_items( missing.subItems, name, missing.count, player, missing_items );
-									end
-								end
-								--log_keys( missing_items );
-								if not resolve_needs( missing_items ) then
-									local items = formatMissing( missing_items, player, 0 );
-									if( items ) then
-										player.print( { "missing-ingredient" } );
-										local n;
-										for n=1,#items do
-											--log_keys( items[n] );
-											player.print( items[n] );
-										end
-										return nil;
-									end
-								end
-								--log( "product:"..num.."  "..product.name .. " item:".. item.name.. " place:"..item.place_result.name );
-								
-								return rname;
-							end
-						end
-					else
-						for num,product in pairs(recipe.products) do
-							if( product.name == item.name ) then
-								player.print( { "cannot-hand-craft" } );
-								return nil;
-							end
-						end
-						--log( "Skipping recipe:"..recipe.name.. " cat:".. recipe.category );
-					end
-				end
+			result = doWork( item.name, n, player );
+			if result.error then return result end;
 		end
 	else
-		player.print( { "cannot-hand-craft" } );
+		--player.print( { "cannot-hand-craft" } );
+		return { error = "cannot-hand-craft", recipe=nil };
 	end
-	return nil;
+	return result;
 end
 
-script.on_event("key-point-and-craft", function(event)
-  local player = game.players[event.player_index]
-  local entity = player.selected
-  if not entity then
-     return;
-  end
-  if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
-     --log( "Have a stack in hand?".. player.cursor_stack.name );
-    player.print( { "must-be-empty" } );
-    return;
-  end
-  if( entity.name == "item-on-ground" ) then
-    player.print( { "must-be-built" } );
-    return;
-  end
-  --log( "craft a thing?".. entity.name );
-  if entity then
-	local recipe = findRecipe( entity.name, 1, player );
-	if recipe then
-		--log( "craft... ".. recipe );
-		player.begin_crafting{ count=1, recipe=recipe, silent=false };
+function craftThing( entity, count, player ) 
+	if not next( item_recipe ) then
+		init_item_recipe_map();
 	end
-  end
+
+	local result = nil;
+	if not entity then
+		 return;
+	end
+
+	if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
+		 --log( "Have a stack in hand?".. player.cursor_stack.name );
+		player.print( { "must-be-empty" } );
+		return;
+	end
+
+	local entName = entity.name;
+	if( entity.name == "item-on-ground" ) then
+		local stack = entity.stack;
+		if stack.valid_for_read then 
+			--log( " item stack name:"..stack.name.. " has ".. stack.count.." type?"..stack.type .. " pro:"..stack.prototype.name);
+			result = doWork( stack.name, count, player );
+			
+			if result.error then
+				player.print( {"",{ result.error }," ",stack.prototype.localised_name } );
+				if result.detail then
+					local items = result.detail;
+					local n;
+					for n=1,#items do
+						--log_keys( items[n] );
+						player.print( items[n] );
+					end
+				end
+				return;
+			end
+			--if result.recipe then
+			--	log( "on-ground recipe name:"..tostring(result.recipe.name) );
+			--end
+		else			
+			player.print( { "fault-identifying-ground-item" } );
+			return;
+		end
+		--	 player.print( { "must-be-built" } );
+		--return;
+	else 
+		if( entity.name == "entity-ghost" ) then
+			--log( "type:"..entity.ghost_type );
+			entName = entity.ghost_name;
+		end
+		if entName then
+			--log( "craft a thing?".. tostring( entName ) );
+			result = findRecipe( entName, count, player );
+			if not result then
+				player.print( {"",{ "cannot-hand-craft" }," ",{"entity-name."..entName} } );
+			end
+		end
+	end
+	--log( "result:".. tostring(result.error).. " r:".. tostring(result.recipe ) );
+
+	if result.recipe then
+		--log( "craft... ".. recipe );
+		player.begin_crafting{ count=count, recipe=result.recipe.name, silent=false };
+	else
+		player.print( {"",{ result.error }," ",{"entity-name."..entName} } );
+		if result.detail then
+			local items = result.detail;
+			local n;
+			for n=1,#items do
+				--log_keys( items[n] );
+				player.print( items[n] );
+			end
+		end
+	end
+end
+
+
+script.on_event("key-point-and-craft", function(event)
+	local player = game.players[event.player_index]
+	local entity = player.selected
+	craftThing( entity, 1, player );
 end)
 
 script.on_event("key-point-and-craft-5", function(event)
-  local player = game.players[event.player_index]
-  local entity = player.selected
-  if not entity then
-     return;
-  end
-  if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
-     --log( "Have a stack in hand?".. player.cursor_stack.name );     
-    player.print( { "must-be-empty" } );
-     return;
-  end
-  if( entity.name == "item-on-ground" ) then
-    player.print( { "must-be-built" } );
-    return;
-  end
-  if entity then
-	local recipe = findRecipe( entity.name, 5, player );
-        if recipe then
-		--log( "craft... 5x".. recipe );
-		player.begin_crafting{ count=5, recipe=recipe, silent=false };
-	end
-  end
+	local player = game.players[event.player_index]
+	local entity = player.selected
+	craftThing( entity, 5, player );
 end)
+
