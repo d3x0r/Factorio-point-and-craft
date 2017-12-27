20108a2e9388b17cce8788486fd18918ab250c75
d3x0r <d3x0r@no-reply.github.com>
2017-12-27 03:31:48 -0800

    Fix default key bind

--- data.lua
+++ data.lua
@@ -3,13 +3,13 @@ data:extend({
   {
     type = "custom-input",
     name = "key-point-and-craft",
-    key_sequence = "ALT + Left Mouse Button",
+    key_sequence = "ALT + mouse-button-1",
     consuming = "script-only"
   },
   {
     type = "custom-input",
     name = "key-point-and-craft-5",
-    key_sequence = "SHIFT + middle mouse button",
+    key_sequence = "SHIFT + mouse-button-3",
     consuming = "script-only"
   }
 })
